88d14b2dc1dbf16525142fc7fa6b55163936d9c0
SashaProfessional <sashaart.97@gmail.com>
2023-07-16 11:25:48 +0300

    Enabled Decimals

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ import { KneeInfo } from './types';
 import './App.css';
 
 function App() {
-  const [balance, setBalance] = useState<Decimal | null>(null)
-  const [percent, setPercent] = useState<Decimal | null>(null)
-  const [kneeQuantity, setKneeQuantity] = useState<Decimal | null>(null)
-  const [multiplier, setMultiplier] = useState<Decimal | null>(null)
+  const [balance, setBalance] = useState("")
+  const [percent, setPercent] = useState("")
+  const [kneeQuantity, setKneeQuantity] = useState("")
+  const [multiplier, setMultiplier] = useState("")
 
   const [target, setTarget] = useState<number | null>(null)
   const [roe, setRoe] = useState<number | null>(null)
@@ -29,10 +29,13 @@ function App() {
       return;
     }
 
-    const newTarget = new Decimal(balance).mul(percent).dividedBy(100).toDP(2)
+    const balanceDecimal = new Decimal(balance)
+    const multiplierDecimal = new Decimal(multiplier)
+
+    const newTarget = balanceDecimal.mul(percent).dividedBy(100).toDP(2)
 
     const newKneeInfos: KneeInfo[] = []
-    const b1 = balance.mul(new Decimal(1).sub(multiplier)).dividedBy(new Decimal(1).sub(multiplier.toPower(kneeQuantity)))
+    const b1 = balanceDecimal.mul(new Decimal(1).sub(multiplierDecimal)).dividedBy(new Decimal(1).sub(multiplierDecimal.toPower(kneeQuantity)))
 
     if (b1.isNaN()) {
       setTarget(null)
@@ -41,13 +44,13 @@ function App() {
       setIsError(true)
     }
 
-    for (let i = 0; i < kneeQuantity.toNumber(); i++) {
+    for (let i = 0; i < +kneeQuantity; i++) {
       const index = new Decimal(i + 1)
 
       newKneeInfos.push({
         index: index.toNumber(),
-        margin: b1.mul(multiplier.toPower(index.minus(1))).toDP(2).toNumber(),
-        sum: b1.mul(new Decimal(1).minus(multiplier.toPower(index))).dividedBy(new Decimal(1).minus(multiplier)).toDP(2).toNumber()
+        margin: b1.mul(multiplierDecimal.toPower(index.minus(1))).toDP(2).toNumber(),
+        sum: b1.mul(new Decimal(1).minus(multiplierDecimal.toPower(index))).dividedBy(new Decimal(1).minus(multiplier)).toDP(2).toNumber()
       })
 
     }
@@ -58,37 +61,50 @@ function App() {
   }, [balance, percent, kneeQuantity, multiplier])
 
   const onBalanceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBalance(getValidatedNumberInput(e.target.value))
+    setBalance(getValidatedNumberInput(e))
   }
 
   const onPercentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPercent(getValidatedNumberInput(e.target.value, 2, 1000))
+    setPercent(getValidatedNumberInput(e, 2, 1000))
   }
 
   const onKneeQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setKneeQuantity(getValidatedNumberInput(e.target.value, 0, 20))
+    setKneeQuantity(getValidatedNumberInput(e, 0, 20))
   }
 
   const onMultiplierChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMultiplier(getValidatedNumberInput(e.target.value))
+    setMultiplier(getValidatedNumberInput(e))
   }
 
-  const getValidatedNumberInput = (value: string, decimalPoints = 2, max = Number.MAX_SAFE_INTEGER) => {
+  const getValidatedNumberInput = (e: React.ChangeEvent<HTMLInputElement>, decimalPoints = 2, max = Number.MAX_SAFE_INTEGER) => {
+    let value = e.target.value
+
     if (!value) {
-      return null;
+      return "";
     }
 
-    let parcedValue = new Decimal(value).toDP(decimalPoints)
-
-    if (parcedValue.lessThan(0)) {
-      parcedValue = parcedValue.abs()
+    if (value[0] === '.') {
+      return "";
     }
 
-    if (parcedValue.greaterThan(max)) {
-      return new Decimal(max)
+    if (value.includes('-')) {
+      value = value.replaceAll('-', '')
     }
 
-    return parcedValue
+    let lastDotIndex = value.lastIndexOf('.')
+
+    while (value.indexOf('.') !== value.lastIndexOf('.')) {
+      lastDotIndex = value.lastIndexOf('.')
+      value = value.substring(0, lastDotIndex - 1) + value.substring(lastDotIndex, value.length)
+    }
+
+    let parcedValue = new Decimal(value)
+
+    if (parcedValue.decimalPlaces() > decimalPoints) {
+      value = parcedValue.toDP(decimalPoints).toString()
+    }
+
+    return value
   }
 
   return (
@@ -96,19 +112,19 @@ function App() {
       <div className='inputs-wrapper'>
         <div className='input-wrapper'>
           <span>Баланс</span>
-          <input type="number" inputMode="numeric" value={balance?.toNumber() ?? ''} onChange={onBalanceChange} />
+          <input type="number" inputMode="numeric" value={balance} onChange={onBalanceChange} />
         </div>
         <div className='input-wrapper'>
           <span>Процент</span>
-          <input type="number" inputMode="numeric" value={percent?.toNumber() ?? ''} onChange={onPercentChange} />
+          <input type="number" inputMode="numeric" value={percent} onChange={onPercentChange} />
         </div>
         <div className='input-wrapper'>
           <span>Колени</span>
-          <input type="number" inputMode="numeric" value={kneeQuantity?.toNumber() ?? ''} onChange={onKneeQuantityChange} />
+          <input type="number" inputMode="numeric" value={kneeQuantity} onChange={onKneeQuantityChange} />
         </div>
         <div className='input-wrapper'>
           <span>Мн. колен</span>
-          <input type="number" inputMode="numeric" value={multiplier?.toNumber() ?? ''} onChange={onMultiplierChange} />
+          <input type="number" inputMode="numeric" value={multiplier} onChange={onMultiplierChange} />
         </div>
       </div>
 
